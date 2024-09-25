a654c1c9bf12995e8b2606736509d183b265de45
Renato M <rdevelops32@gmail.com>
2024-09-25 09:24:27 -0500

    add live caption description

--- src/events/ready.event.ts
+++ src/events/ready.event.ts
@@ -1,9 +1,13 @@
-import { Events, Client } from 'discord.js'
+import { Events, Client, ActivityType } from 'discord.js'
 
 export default {
   name: Events.ClientReady,
   once: true,
   controller(client: Client) {
     console.log(`Ready! Logged in as ${client.user.tag}`)
+    client.user.setActivity({
+      name: 'Escribe /ayuda para mas informacion.',
+      type: ActivityType.Custom
+    })
   }
 }
