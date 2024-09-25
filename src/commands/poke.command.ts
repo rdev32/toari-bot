3c5dd0aae16f5e5c90c4eba721ea9920f675d661
Renato M <rdevelops32@gmail.com>
2024-09-25 11:04:50 -0500

    mv poke

--- src/commands/poke.command.ts
+++ src/commands/poke.command.ts
@@ -2,9 +2,17 @@ import { CommandInteraction, SlashCommandBuilder } from 'discord.js'
 
 export default {
   data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with pong'),
+    .setName('poke')
+    .setDescription('Envia un toque a alguien aleatorio'),
   service: async (interaction: CommandInteraction) => {
-    await interaction.reply('Pong!')
+    const members = await interaction.guild.members.fetch()
+    const users: Array<string> = []
+    members.forEach((member) => users.push(member.displayName))
+    const randomIndex = Math.floor(Math.random() * users.length)
+    const target = users[randomIndex]
+    await interaction.reply({
+      content: `Enviaste un toque a ${target}`,
+      ephemeral: true
+    })
   }
 }
